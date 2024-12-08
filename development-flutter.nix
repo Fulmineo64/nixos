fd218496a6fbfecdfcbf28faf13357fbf65eb4b7
Nicholas Dartora <dartora.nicholas@gmail.com>
2024-12-08 13:45:29 +0100

    Updated flutter configuration

--- development-flutter.nix
+++ development-flutter.nix
@@ -34,12 +34,24 @@ in
     flutter
     (android-studio.withSdk androidComposition.androidsdk)
     google-chrome
-	  temurin-bin
+	  temurin-bin # required for android studio
+    gcc # required to compile encore
+    docker # optional - for encore examples
+    postgresql # Database
+    pgadmin4-desktopmode # Database administration console
   ];
 
   environment.variables = {
     CHROME_EXECUTABLE = "${pkgs.google-chrome}/bin/google-chrome-stable";
     ANDROID_SDK_ROOT = "${androidComposition.androidsdk}/libexec/android-sdk";
+    ENCORE_RUNTIMES_PATH="/home/nicholas/.encore/encore/runtimes";
+    ENCORE_GOROOT="/home/nicholas/.encore/encore-go/go";
+  };
+
+  environment.sessionVariables = {
+    PATH = [
+      "/home/nicholas/.encore/encore"
+    ];
   };
 
   nixpkgs.config.android_sdk.accept_license = true;
